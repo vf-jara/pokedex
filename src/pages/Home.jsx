cef8e0f8a4e080668a2f1a3eb09151fe88254ba0
Vinicius Jara <viniciusfjara@gmail.com>
2022-09-14 15:00:21 -0400

    adiciona navegação básica

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,32 +4,49 @@ import { Container } from '../styles'
 
 export default function Home() {
     const [list, setList] = useState([])
+    const [page, setPage] = useState("")
+    const [prevPage, setPrevPage] = useState()
+    const [nextPage, setNextPage] = useState()
     useEffect(() => {
         getPokemonsList()
-    }, [])
+    }, [page])
 
     const getPokemonsList = () => {
         axios({
-            url: 'https://pokeapi.co/api/v2/pokemon/',
+            url: page,
+            baseURL: 'https://pokeapi.co/api/v2/pokemon/',
             method: 'get',
             params: {
                 limit: 50,
             }
         }).then((res) => {
+            console.log(res)
             setList(res.data.results)
-            console.log(list)
+            setPrevPage(res.data.previous)
+            setNextPage(res.data.next)
         })
     }
+
+    function nextPageNav() {
+        setPage(nextPage)
+    }
+    function prevPageNav() {
+        setPage(prevPage)
+    }
+
+
     return (
         <Container>
             <h1>Pokemon List</h1>
             <ul>
                 {
                     list.map((pokemon) => (
-                        <li><p>{pokemon.name}</p></li>
+                        <li key={pokemon.name}><p>{pokemon.name}</p></li>
                     ))
                 }
             </ul>
+            <button onClick={prevPageNav}>Prev</button>
+            <button onClick={nextPageNav}>Next</button>
         </Container>
     )
 }
--- src/styles.js
+++ src/styles.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
     min-height: 100vh;
     justify-content:center;
     padding:0;
